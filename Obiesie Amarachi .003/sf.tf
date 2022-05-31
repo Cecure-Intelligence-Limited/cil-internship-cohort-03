5705b569e3c73662818c019fc7de339f4bdf3cda
Amara1997 <60925124+Davina-hub@users.noreply.github.com>
2022-05-31 09:47:34 +0100

    Update sf.tf

--- Obiesie Amarachi .003/sf.tf
+++ Obiesie Amarachi .003/sf.tf
@@ -23,16 +23,16 @@ EOF
 
   service_integrations = {
     dynamodb = {
-      dynamodb = ["arn:aws:dynamodb:eu-west-1:052212379155:table/Test"]
+      dynamodb = ["arn:aws:dynamodb:eu-east-1:052212379155:table/Test"]
     }
 
     lambda = {
-      lambda = ["arn:aws:lambda:eu-west-1:123456789012:function:test1", "arn:aws:lambda:eu-west-1:123456789012:function:test2"]
+      lambda = ["arn:aws:lambda:us-east-1:970025564377:function:myFirstFunction", "arn:aws:lambda:us-east-1:970025564377:function:myFirstFunction1"]
     }
 
     stepfunction_Sync = {
-      stepfunction = ["arn:aws:states:eu-west-1:123456789012:stateMachine:test1"]
-      stepfunction_Wildcard = ["arn:aws:states:eu-west-1:123456789012:stateMachine:test1"]
+      stepfunction = ["arn:aws:states:eu-east-1:123456789012:stateMachine:test1"]
+      stepfunction_Wildcard = ["arn:aws:states:eu-east-1:123456789012:stateMachine:test1"]
 
       # Set to true to use the default events (otherwise, set this to a list of ARNs; see the docs linked in locals.tf
       # for more information). Without events permissions, you will get an error similar to this:
